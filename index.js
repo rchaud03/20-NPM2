b647a738730c98d64e5154890486caa4e709928f
ronald chaudry <rchaud03@gmail.com>
2018-12-19 06:33:09 -0500

    minor fixes

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ let city = argv.c || "Miami";
 let queryUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
 request(queryUrl, function (err, response, body) {
-  if(err){
+  if (err) {
     console.log('error:', error);
   }
   else {
     let response = JSON.parse(body)
-    let query = `It's currently ${response.weather[0].main} and  a temperature of ${response.main.temp} degrees with a humidity of ${response.main.humidity} in ${response.name}!`;
+    let query = `We currently have ${response.weather[0].main}/${response.weather[0].description} with  a temperature of ${response.main.temp} degrees and a humidity of ${response.main.humidity} in ${response.name}!`;
     console.log(query);
   }
 });
